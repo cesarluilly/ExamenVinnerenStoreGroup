ac7635066196924cfcba60dcc9dfe47fad406066
Cesar Luillly <cesarluilly.garcia@hotmail.com>
2023-01-22 23:26:13 -0600

    Create interface category dominio, applicacion, controller empty

---
+++ Vinneren.Storegp/Vinneren.Storegp.Application.Interface/ICategoryApplication.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//                                                          //AUTHOR:  (CLGA - Cesar Garcia).
+//                                                          //CO-AUTHOR:  (-).
+//                                                          //DATE: January 27, 2023. 
+namespace Vinneren.Storegp.Application.Interface
+{
+    //==================================================================================================================
+    public interface ICategoryApplication
+    {
+
+    }
+
+    //==================================================================================================================
+}
---
+++ Vinneren.Storegp/WebApplication1Vinneren.Storegp.Service.WebApi/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+//                                                          //AUTHOR:  (CLGA - Cesar Garcia).
+//                                                          //CO-AUTHOR:  (-).
+//                                                          //DATE: January 22, 2023. 
+namespace WebApplication1Vinneren.Storegp.Service.WebApi.Controllers
+{
+    //==================================================================================================================
+    [Route("api/[controller]")]
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
